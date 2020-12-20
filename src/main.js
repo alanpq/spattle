494ca419eed93e3f7ce20fb8c944949b788d1338
cakyman <alanpanayotov@gmail.com>
2020-12-20 08:55:13 +0000

    check for existing token and redirect

--- src/main.js
+++ src/main.js
@@ -7,43 +7,6 @@ import Battle from './battle.vue'
 
 import "regenerator-runtime"
 
-
-const loadOrUpdate = async () => {
-  if (window.location.search) { // maybe new oauth response?
-    const params = new URLSearchParams(window.location.search)
-    if (params.get('code')) {
-      const formData = new FormData();
-      formData.append("client_id", "3a25adc518944ba0b50c6a1376ab6a8a") // TODO: fetch client id from server
-      formData.append("grant_type", "authorization_code")
-      formData.append("code", params.get('code'))
-      formData.append("redirect_uri", "http://localhost:8080") // TODO: change redirect url
-      console.log("verifier:", localStorage.getItem("verifier"))
-      formData.append("code_verifier", localStorage.getItem("verifier"))
-      const res = await (fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams(formData).toString(),
-      }).then(res => res.json()))
-      console.log(res);
-      localStorage.setItem("access_token", res.access_token)
-      localStorage.setItem("aquired_at", Date.now())
-      localStorage.setItem("expires_in", res.expires_in * 1000)
-      localStorage.setItem("refresh_token", res.refresh_token)
-    } else {
-      console.error(params.get('error'))
-    }
-    history.replaceState({}, "", location.protocol + '//' +
-      location.host +
-      location.pathname)
-  }
-}
-
-window.onpopstate = loadOrUpdate;
-window.onload = loadOrUpdate;
-
-
 const routes = {
   '/': Index,
   '/battle': Battle
@@ -66,6 +29,61 @@ const router = new VueRouter(
     ]
   }
 )
+
+const haveToken = () => {
+  const acquired_at = localStorage.getItem("acquired_at");
+  if (!acquired_at) return false;
+  // TODO: check if we can refresh token after expiry
+  if (Date.now() < parseInt(acquired_at) + parseInt(localStorage.getItem("expires_in"))) {
+    return true;
+  } else { // we need to refresh token
+    // TODO: token refreshing
+    return false;
+  }
+}
+
+const loadOrUpdate = async () => {
+  if (window.location.search) { // maybe new oauth response?
+    const params = new URLSearchParams(window.location.search)
+    if (params.get('code')) {
+      const formData = new FormData();
+      formData.append("client_id", "3a25adc518944ba0b50c6a1376ab6a8a") // TODO: fetch client id from server
+      formData.append("grant_type", "authorization_code")
+      formData.append("code", params.get('code'))
+      formData.append("redirect_uri", "http://localhost:8080") // TODO: change redirect url
+      console.log("verifier:", localStorage.getItem("verifier"))
+      formData.append("code_verifier", localStorage.getItem("verifier"))
+      const res = await (fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams(formData).toString(),
+      }).then(res => res.json()))
+      console.log(res);
+      localStorage.setItem("access_token", res.access_token)
+      localStorage.setItem("acquired_at", Date.now())
+      localStorage.setItem("expires_in", res.expires_in * 1000)
+      localStorage.setItem("refresh_token", res.refresh_token)
+    } else {
+      console.error(params.get('error'))
+    }
+    history.replaceState({}, "", location.protocol + '//' +
+      location.host +
+      location.pathname)
+  }
+
+  if (router.currentRoute == "/" && haveToken()) {
+    console.log("already have token")
+    router.push("/battle")
+  }
+}
+
+window.onpopstate = loadOrUpdate;
+window.onload = loadOrUpdate;
+
+
+
 
 Vue.use(VueRouter)
 const app = new Vue({
